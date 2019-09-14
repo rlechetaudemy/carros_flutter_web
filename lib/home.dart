55049af1337b06297cdb414d0babfdc69348425b
rlecheta <rlecheta@livetouch.com.br>
2019-09-14 20:37:31 -0300

    Fist Commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:carros_flutter_web/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -10,63 +9,79 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Size get size => MediaQuery.of(context).size;
 
-  bool get showDrawer => size.width <= 760;
-
-  //Text("Flutter Web - ${size.width}/${size.height}")
+  bool get showMenu => size.width > 760;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Container(
-            color: Colors.blue,
-            width: size.width,
-            height: 80,
-          ),
-          Container(
-            width: size.width,
-            height: size.height - 80,
-            child: Row(
-              children: <Widget>[
-                showDrawer ? Container() : _menu(),
-                _right()
-              ],
-            ),
+          _header(),
+          _body(),
+        ],
+      ),
+    );
+  }
+
+  _header() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      color: Colors.blue,
+      width: size.width,
+      height: 80,
+      child: Row(
+        children: <Widget>[
+          Text(
+            "Flutter Web - ${size.width}/${size.height}",
+            style: TextStyle(color: Colors.white),
           ),
         ],
       ),
     );
   }
 
+  _body() {
+    return Container(
+      width: size.width,
+      height: size.height - 80,
+      child: showMenu
+          ? Row(
+              children: <Widget>[
+                _menu(),
+                _right(),
+              ],
+            )
+          : _right(),
+    );
+  }
+
   _menu() {
     return Container(
-        width: menuWidth,
-        color: Colors.blue[100],
-        child: ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.star),
-              title: Text("Item 1"),
-            ),
-            ListTile(
-              leading: Icon(Icons.star),
-              title: Text("Item 2"),
-            ),
-            ListTile(
-              leading: Icon(Icons.star),
-              title: Text("Item 3"),
-            )
-          ],
-        ),
-      );
+      width: menuWidth,
+      color: Colors.blue[100],
+      child: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text("Item 1"),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text("Item 2"),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text("Item 3"),
+          )
+        ],
+      ),
+    );
   }
 
   _right() {
-
     return Container(
-        width: showDrawer ? size.width : size.width - menuWidth,
-        color: Colors.grey,
-      );
+      width: showMenu ? size.width - menuWidth :  size.width,
+      color: Colors.grey,
+    );
   }
 }
