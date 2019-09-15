816faded7ffa62a99a50e06885c6bc76baa41821
rlecheta <rlecheta@livetouch.com.br>
2019-09-15 09:59:55 -0300

    nav drawer

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+
+double menuWidth = 230;
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,4 @@
-
+import 'package:carros_flutter_web/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,34 +7,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Size get size => MediaQuery.of(context).size;
+
+  bool get showDrawer => size.width <= 760;
+
   @override
   Widget build(BuildContext context) {
-
-    Size size = MediaQuery.of(context).size;
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Web - ${size.width}/${size.height}"),
+        automaticallyImplyLeading: showDrawer,
       ),
       body: _body(),
+      drawer: Drawer(
+        child: _menu(),
+      ),
     );
   }
 
   _body() {
+    return showDrawer
+        ? _right()
+        : Row(
+            children: <Widget>[
+              _menu(),
+              _right(),
+            ],
+          );
+  }
 
-    Size size = MediaQuery.of(context).size;
+  _menu() {
+    return Container(
+      width: menuWidth,
+      color: Colors.blue[100],
+      child: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text("Item 1"),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text("Item 2"),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text("Item 3"),
+          )
+        ],
+      ),
+    );
+  }
 
-    return Row(
-      children: <Widget>[
-        Container(
-          width: 230,
-          color: Colors.blue[100],
-        ),
-        Container(
-          width: size.width - 230,
-          color: Colors.grey,
-        )
-      ],
+  _right() {
+    return Container(
+      width: showDrawer ? size.width : size.width - menuWidth,
+      color: Colors.grey,
     );
   }
 }
