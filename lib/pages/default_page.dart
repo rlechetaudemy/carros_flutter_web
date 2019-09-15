bf2b763f7d3c1b1cca16a84cc0aed49ac97f30ba
rlecheta <rlecheta@livetouch.com.br>
2019-09-15 16:51:42 -0300

    Header

---
+++ lib/pages/default_page.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class DefaultPage extends StatefulWidget {
+  @override
+  _DefaultPageState createState() => _DefaultPageState();
+}
+
+class _DefaultPageState extends State<DefaultPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Center(
+      child: Text("Home"),
+    );
+  }
+}
