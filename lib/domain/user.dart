4b687d0f673c7f1f604269c686ea10b8eb1ab1bf
rlecheta <rlecheta@livetouch.com.br>
2019-09-15 11:34:28 -0300

    Header

---
+++ lib/domain/user.dart
@@ -0,0 +1,12 @@
+
+final user = Usuario(
+  "Admin",
+  "https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg",
+);
+
+class Usuario {
+  String nome;
+  String urlFoto;
+
+  Usuario(this.nome, this.urlFoto);
+}
