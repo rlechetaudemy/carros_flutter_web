1c5af79dff9ef6fbedc1d58749ef3a6e20b38d8a
rlecheta <rlecheta@livetouch.com.br>
2019-09-23 13:59:28 -0300

    Carros grid

--- lib/app_model.dart
+++ lib/app_model.dart
@@ -1,21 +1,28 @@
 
 import 'package:carros_flutter_web/pages/carros/carros_page.dart';
+import 'package:carros_flutter_web/pages/default_page.dart';
 import 'package:flutter/material.dart';
 
 class AppModel extends ChangeNotifier {
 
   List<Widget> pages = [];
 
+  Widget defaultPage = DefaultPage();
+
   AppModel() {
-    pages.add(CarrosPage());
+    pages.add(defaultPage);
   }
 
   push(Widget page, {bool replace = false}) {
     if(replace) {
       this.pages.clear();
+
+      pages.add(defaultPage);
     }
 
-    this.pages.add(page);
+    if(page.runtimeType.toString() != defaultPage.runtimeType.toString()) {
+      this.pages.add(page);
+    }
 
     notifyListeners();
   }
