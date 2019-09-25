27d83bef11ef4fb7579dc302112413899eae0a1b
rlecheta <rlecheta@livetouch.com.br>
2019-09-24 22:26:49 -0300

    aula 33

--- lib/web/breadcrumb.dart
+++ lib/web/breadcrumb.dart
@@ -1,6 +1,7 @@
 
 import 'package:carros_flutter_web/app_model.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
 class BreadCrumb extends StatefulWidget {
@@ -13,6 +14,23 @@ class _BreadCrumbState extends State<BreadCrumb> {
   Widget build(BuildContext context) {
     AppModel app = Provider.of<AppModel>(context);
 
-    return Text("${app.pages.toString()}");
+    return ListView.builder(
+      itemCount: app.pages.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+
+        PageInfo info = app.pages[index];
+
+        return Row(
+          children: <Widget>[
+            ConstrainedBox(
+              constraints: BoxConstraints.expand(width: 32),
+              child: Icon(index == 0 ? FontAwesomeIcons.home : FontAwesomeIcons.chevronRight),
+            ) ,
+            Text(info.title, style: TextStyle(fontSize: 20),)
+          ],
+        );
+
+    },);
   }
 }
